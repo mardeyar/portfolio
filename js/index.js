4e2296739d404f1074188370aa7f8e2cedd22f4c
Mark Deyarmond <deyarmondmark@gmail.com>
2023-11-09 17:32:17 -0400

    Moved to folder css for organization

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
-// Fade in animation for transition into index.html
+// Fade in animation for transition into index and it's sections
 document.addEventListener('DOMContentLoaded', function() {
-    setTimeout(function() {
-        document.body.classList.add('fade-in');
-    }, 100);
+    const pathName = window.location.pathname;
+    const aboutMeSection = document.querySelector('.container #about-me');
+    const skillsSection = document.querySelector('.container #tech');
+
+    if (pathName.endsWith("index.html")) {
+        setTimeout(function() {
+            aboutMeSection.classList.add('fade-in');
+        }, 500);
+    } else if (pathName.endsWith("tech.html")) {
+        setTimeout(function() {
+            skillsSection.classList.add('fade-in');
+        }, 500);
+    }
 });
