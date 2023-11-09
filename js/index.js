c14f40e676752007036ff7483ef468818693b748
Mark Deyarmond <deyarmondmark@gmail.com>
2023-11-08 22:29:13 -0400

    Added more CSS animations

---
+++ js/index.js
@@ -0,0 +1,6 @@
+// Fade in animation for transition into index.html
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        document.body.classList.add('fade-in');
+    }, 100);
+});
---
+++ js/landing.js
@@ -0,0 +1,23 @@
+// Typewriter animation for the call to action
+function typeWriter() {
+    var typeText = document.getElementById("type-text");
+    typeText.style.visibility = "hidden";
+    setTimeout(function() {
+        typeText.style.visibility = "visible";
+    }, 4000);
+}
+
+window.addEventListener('load', typeWriter);
+
+// Fade out animation for transition out of landing.html
+document.addEventListener('DOMContentLoaded', function () {
+    const transitionLink = document.querySelector('a[href="index.html"]');
+    
+    transitionLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.body.classList.add('fade-out');
+        setTimeout(function () {
+            window.location.href = transitionLink.getAttribute('href');
+        }, 1000);
+    });
+});
