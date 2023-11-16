41b9983cb7ff073613178ce400413d553df44627
Mark Deyarmond <deyarmondmark@gmail.com>
2023-11-15 21:22:43 -0400

    Introduced fade out & fade in effect transitions

--- js/index.js
+++ js/index.js
@@ -1,16 +1,44 @@
-// Fade in animation for transition into index and it's sections
 document.addEventListener('DOMContentLoaded', function() {
     const pathName = window.location.pathname;
-    const aboutMeSection = document.querySelector('.container #about-me');
+    const aboutMeSection = document.querySelector('.container #index');
     const skillsSection = document.querySelector('.container #tech');
+    const projectSection = document.querySelector('.container #projects');
+    const contactSection = document.querySelector('.container #contact');
 
-    if (pathName.endsWith("index.html")) {
-        setTimeout(function() {
-            aboutMeSection.classList.add('fade-in');
-        }, 500);
-    } else if (pathName.endsWith("tech.html")) {
-        setTimeout(function() {
-            skillsSection.classList.add('fade-in');
+    function fadeIn(section) {
+        section.style.display = 'block';
+        section.classList.remove('fade-out');
+        section.classList.add('fade-in');
+    }
+
+    function fadeOut(link, sectionOut) {
+        sectionOut.classList.add('fade-out');
+        setTimeout(() => {
+            window.location.href = link.href;
         }, 500);
     }
+
+    const sections = [aboutMeSection, skillsSection, projectSection, contactSection];
+    const navLinks = document.querySelectorAll('.nav-links a');
+
+    sections.forEach(section => {
+        if (section) {
+            if (pathName.endsWith(section.id + '.html')) {
+                setTimeout(function() {
+                    fadeIn(section);
+                }, 500);
+            }
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            sections.forEach(section => {
+                if (section) {
+                    fadeOut(link, section);
+                }
+            });
+        });
+    });
 });
--- js/projects.js
+++ js/projects.js
@@ -5,17 +5,22 @@ document.addEventListener('DOMContentLoaded', function() {
         button.addEventListener('click', function() {
             var workBox = button.closest('.work-box');
             toggleContentVisibility(workBox);
+            toggleButtonText(button);
         });
     });
 
     function toggleContentVisibility(workBox) {
-        var paragraph = workBox.querySelectorAll('p:not(#title)');
-        var link = workBox.querySelector('a');
+        var paragraph = workBox.querySelectorAll('.work-box p:not(#title)');
+        var link = workBox.querySelector('.work-box a');
 
         paragraph.forEach(function (paragraph) {
             paragraph.style.display = (paragraph.style.display === 'none' || paragraph.style.display === '') ? 'block' : 'none';
         });
 
         link.style.display = (link.style.display === 'none' || link.style.display === '') ? 'block' : 'none';
+    }
+
+    function toggleButtonText(button) {
+        button.textContent = (button.textContent === 'Show more') ? 'Show less' : 'Show more';
     }
 });
