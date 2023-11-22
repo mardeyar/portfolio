a2adc00676703687888928133d15ff3d4e4dcf7a
Mark Deyarmond <deyarmondmark@gmail.com>
2023-11-22 08:50:00 -0400

    Add support for expanding box on touchscreen

--- js/projects.js
+++ js/projects.js
@@ -2,18 +2,24 @@ document.addEventListener('DOMContentLoaded', function() {
     var expand = document.querySelectorAll('.expand');
 
     expand.forEach(function(button) {
-        button.addEventListener('click', function() {
-            var workBox = button.closest('.work-box');
-            toggleContentVisibility(workBox);
-            toggleButtonText(button);
-        });
+        button.addEventListener('click', toggleContent);
+        button.addEventListener('touchstart', toggleContent);
     });
+
+    function toggleContent(event) {
+        event.preventDefault();
+
+        var button = event.target;
+        var workBox = button.closest('.work-box');
+        toggleContentVisibility(workBox);
+        toggleButtonText(button);
+    }
 
     function toggleContentVisibility(workBox) {
         var paragraph = workBox.querySelectorAll('.work-box p:not(#title)');
         var link = workBox.querySelector('.work-box a');
 
-        paragraph.forEach(function (paragraph) {
+        paragraph.forEach(function(paragraph) {
             paragraph.style.display = (paragraph.style.display === 'none' || paragraph.style.display === '') ? 'block' : 'none';
         });
 
